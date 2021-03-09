a31e2051dfe9491316b013967b88d77465c43289
Carl Rosales <rosales.carlangel@gmail.com>
2021-03-09 17:31:02 +0800

    solution for gilded rose, rspec exercise (avion)

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,49 +1,71 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
+    item_type = get_item_type(item)
+    quality_limit_reached = past_quality_limits?(item, item_type)
+
+    unless item_type == "Legendary Item"
+      unless quality_limit_reached
+        item.quality += get_quality_increment(item, item_type)
+        item.quality = 0 if item.quality < 0
       end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
       item.sell_in -= 1
     end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
   end
+end
+
+#RETURNS ITEM TYPE
+def get_item_type(item)
+  case
+    when item.name.include?("Aged Brie")
+      "Aged Brie"
+    when item.name.include?("Sulfuras")
+      "Legendary Item"
+    when item.name.include?("Backstage")
+      "Backstage Pass"
+    when item.name.include?("Conjured")
+      "Conjured Item"
+    when item.sell_in <= 0
+      "Item Past Sell Date"
+    else
+      "Normal Item"
+  end
+end
+
+#DETERMINES IF ITEM HAS REACHED ITS QUALITY LIMIT
+def past_quality_limits?(item, type)
+  if type == "Aged Brie" || type == "Backstage Pass"
+    item.quality > 49
+  else
+    item.quality < 1
+  end
+end
+
+def get_quality_increment(item, item_type)
+  case item_type
+    when "Aged Brie"
+      1
+    when "Conjured Item"
+      -2
+    when "Backstage Pass"
+      get_pass_increment(item)
+    when "Item Past Sell Date"
+      -2
+    else
+      -1
+    end
+end
+
+def get_pass_increment(pass)
+  case
+    when pass.sell_in.between?(0, 6)
+      3
+    when pass.sell_in.between?(5, 11)
+      2
+    when pass.sell_in > 10
+      1
+    else
+      -pass.quality
+    end
 end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -11,4 +11,162 @@ RSpec.describe "GildedRose" do
      Item.new("Conjured Mana Cake", 3, 6),
     ]
   end
+
+  context "Normal Items" do
+    let!(:items) do
+      [
+        Item.new("+5 Dexterity Vest", 10, 20),
+        Item.new("Elixir of the Mongoose", 5, 7)
+      ]
+    end
+
+    it "should decrease sell_in and quality by 1" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq 9
+      expect(items[0].quality).to eq 19
+
+      expect(items[1].sell_in).to eq 4
+      expect(items[1].quality).to eq 6
+    end
+  end
+
+  context "Items Past Sell Date" do
+    let!(:items) do
+      [
+        Item.new("+5 Dexterity Vest", 0, 20),
+        Item.new("Elixir of the Mongoose", -1, 7)
+      ]
+    end
+
+    it "should decrease sell_in by 1 and quality (twice as fast) by 2" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq -1
+      expect(items[0].quality).to eq 18
+
+      expect(items[1].sell_in).to eq -2
+      expect(items[1].quality).to eq 5
+    end
+  end
+
+  context "Aged Brie" do
+    let!(:items) do
+      [
+        Item.new("Aged Brie", 2, 0)
+      ]
+    end
+
+    it "should decrease sell_in by 1 and increase quality by 1" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq 1
+      expect(items[0].quality).to eq 1
+    end
+  end
+
+  context "Legendary Items" do
+    let!(:items) do
+      [
+        Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+      ]
+    end
+
+    it "should have constant sell_in and quality" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq 0
+      expect(items[0].quality).to eq 80
+    end
+  end
+
+  context "Backstage Passes" do
+    let!(:items) do
+      [
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 7, 20),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 2, 20),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 20)
+      ]
+    end
+
+    it "should decrease sell_in by 1 and increase quality by 1" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq 14
+      expect(items[0].quality).to eq 21
+    end
+
+    it "should decrease sell_in by 1 and increase quality by 2 when sell_in is less than or equal to 10" do
+      update_quality(items)
+
+      expect(items[1].sell_in).to eq 6
+      expect(items[1].quality).to eq 22
+    end
+
+    it "should decrease sell_in by 1 and increase quality by 3 when sell_in is less than or equal to 5" do
+      update_quality(items)
+
+      expect(items[2].sell_in).to eq 1
+      expect(items[2].quality).to eq 23
+    end
+
+    it "should decrease sell_in by 1 and drop quality to 0 when sell_in is less than or equal to 0 (zero or negative)" do
+      update_quality(items)
+
+      expect(items[3].sell_in).to eq -2
+      expect(items[3].quality).to eq 0
+    end
+  end
+
+  context "Conjured Items" do
+    let!(:items) do
+      [
+        Item.new("Conjured Mana Cake", 3, 6)
+      ]
+    end
+
+    it "should decrease sell_in by 1 and quality by 2" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq 2
+      expect(items[0].quality).to eq 4
+    end
+  end
+
+  context "(Lower Limit) Item Quality" do
+    let!(:items) do
+      [
+        Item.new("+5 Dexterity Vest", 0, 0),
+        Item.new("Elixir of the Mongoose", -1, 1)
+      ]
+    end
+
+    it "should never be less than 0 (negative)" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq -1
+      expect(items[0].quality).to eq 0
+
+      expect(items[1].sell_in).to eq -2
+      expect(items[1].quality).to eq 0
+    end
+  end
+
+  context "(Higher Limit) Item Quality" do
+    let!(:items) do
+      [
+        Item.new("Aged Brie", 2, 50)
+      ]
+    end
+
+    it "should never be more than 50" do
+      update_quality(items)
+
+      expect(items[0].sell_in).to eq 1
+      expect(items[0].quality).to eq 50
+    end
+  end
+
+
 end
